71f7d6f1adc26790dc50e2c44a6ed1674e951aac
Sergio Fernandez <s.fernandez01@gmail.com>
2025-08-06 20:50:09 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+let amigos = [];
+const regex = /^[A-Za-z]+$/;
+
+function limpiarLista(id){
+    let lista = document.getElementById(id);   
+    lista.innerHTML = "";
+    return lista;
+}
+
+function crearlista(indice){
+    let lista = document.createElement('li');
+    lista.innerHTML = amigos[indice].charAt(0).toUpperCase() + amigos[indice].slice(1).toLowerCase();
+    return lista;    
+}
+
+
+function agregarAmigo(){
+    let nombreAmigo = (document.getElementById('amigo').value).toLowerCase();    
+    document.getElementById('amigo').value = '';
+    if(!regex.test(nombreAmigo)){
+        alert('Por favor, inserte un nombre valido. ');
+    }else if(amigos.includes(nombreAmigo)){
+        alert('el amigo ingresado esta en la lista.');
+    }else{
+        amigos.push(nombreAmigo);
+        listaAmigos();
+    }    
+}
+
+function listaAmigos(){
+    let lista = limpiarLista('listaAmigos');
+    for(i = 0; i<amigos.length; i++){          
+        lista.appendChild(crearlista(i));
+    }
+    
+}
+
+function sortearAmigo(){
+    if(amigos.length === 0){
+        alert('no se han ingresado amigos');
+    }else{        
+        let numAmigo = Math.floor(Math.random()*(amigos.length));
+        limpiarLista('listaAmigos');        
+        limpiarLista('resultado').appendChild(crearlista(numAmigo));
+        
+    }    
+}
+
+
+
